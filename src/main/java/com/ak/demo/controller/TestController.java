3072adf1bc0390d47a9880eccff6d6fb8037fe1d
Akeung <zzkeung@163.com>
2020-08-21 22:56:48 +0800

    feat

---
+++ src/main/java/com/ak/demo/controller/TestController.java
@@ -0,0 +1,22 @@
+/*
+ * FileName: TestController
+ * Author:   Akeung
+ * Date:     2020/8/21
+ */
+package com.ak.demo.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author Akeung
+ * 2020/8/21
+ */
+@RestController
+public class TestController {
+
+    @GetMapping("hello")
+    public String hello() {
+        return "Hello World";
+    }
+}
